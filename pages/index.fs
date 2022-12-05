1bc60c961274929a2481b578506b242afc52adc3
Jon Roberts <bigjonroberts@gmail.com>
2022-12-05 12:51:18 -0600

    use latest fable and cleanup cruft

--- pages/index.fs
+++ pages/index.fs
@@ -1,36 +1,14 @@
 module Pages.Index
 
-open Fable.React
 open Fable.Core
 open Fable.Core.JsInterop
 
-let inline toJsx (el: ReactElement) : JSX.Element = unbox el
-let inline toReact (el: JSX.Element) : ReactElement = unbox el
-
-/// Enables use of Feliz styles within a JSX hole
-// let inline toStyle (styles: Feliz.IStyleAttribute list) : obj = JsInterop.createObj (unbox styles)
-
-let toClass (classes: (string * bool) list) : string =
-    classes
-    |> List.choose (fun (c, b) ->
-        match c.Trim(), b with
-        | "", _
-        | _, false -> None
-        | c, true -> Some c)        
-    |> String.concat " "
-
-// [<JSX.Component>]
-// let hello () =
-//     JSX.jsx
-//         $"""
-//         <div>hello</div>
-//     """
-
 let home () =
-    div [] [ // toReact (hello())
-             str "Fable welcome Next.js. "
-             br []
-             str "enjoy your stay." ]
-    // html $"""<my-counter initial="10"></my-counter>"""
+    JSX.jsx
+        $"""
+    <div>
+        <p>Hello World</p>
+    </div>
+    """
 
 home |> exportDefault //exportDefault is important for next js
